e6fc4077c858e446b572b2ea2b45065c72c1a8d9
Surendra <manjhisurendra08@gmail.com>
2024-03-26 17:57:27 +0530

    Pattern printing with loops

---
+++ pattern.cpp
@@ -0,0 +1,261 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// *****
+
+	// for (int i = 1; i <= 5; i++)
+	// {
+	// 	cout << "*";
+	// }
+
+	// *****
+	// *****
+	// *****
+	// *****
+	// *****
+
+	// int n = 5;
+
+	// for (int row = 1; row <= n; row++)
+	// {
+	// 	for (int col = 1; col <= n; col++)
+	// 	{
+	// 		cout << "*";
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	// 10 10 10 10 10
+	// 10 10 10 10 10
+	// 10 10 10 10 10
+	// 10 10 10 10 10
+
+	// for (int row = 1; row <= 4; row++)
+	// {
+	// 	for (int col = 1; col <= 5; col++)
+	// 	{
+	// 		cout << "10 ";
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	// 11111
+	// 22222
+	// 33333
+	// 44444
+	// 55555
+
+	// int n = 5;
+
+	// for (int row = 1; row <= n; row++)
+	// {
+	// 	for (int col = 1; col <= n; col++)
+	// 	{
+	// 		cout << row;
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	// 12345
+	// 12345
+	// 12345
+	// 12345
+	// 12345
+
+	// int n = 5;
+
+	// for (int row = 1; row <= n; row++)
+	// {
+	// 	for (int col = 1; col <= n; col++)
+	// 	{
+	// 		cout << col;
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	// 54321
+	// 54321
+	// 54321
+	// 54321
+	// 54321
+
+	// int n = 5;
+
+	// for (int row = 1; row <= n; row++)
+	// {
+	// 	for (int col = n; col >= 1; col--)
+	// 	{
+	// 		cout << col;
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	// 1 4 9 16 25
+	// 1 4 9 16 25
+	// 1 4 9 16 25
+	// 1 4 9 16 25
+	// 1 4 9 16 25
+
+	// int n = 5;
+
+	// for (int row = 1; row <= n; row++)
+	// {
+	// 	for (int col = 1; col <= n; col++)
+	// 	{
+	// 		cout << col * col << " ";
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	// 1 8 27 64 125
+	// 1 8 27 64 125
+	// 1 8 27 64 125
+	// 1 8 27 64 125
+	// 1 8 27 64 125
+
+	// int n = 5;
+
+	// for (int row = 1; row <= n; row++)
+	// {
+	// 	for (int col = 1; col <= n; col++)
+	// 	{
+	// 		cout << col * col * col << " ";
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	// aaaaa
+	// bbbbb
+	// ccccc
+	// ddddd
+	// eeeee
+
+	// int n = 5;
+	// char alp = 'a';
+
+	// for (int row = 1; row <= n; row++)
+	// {
+	// 	for (int col = 1; col <= n; col++)
+	// 	{
+	// 		cout << alp;
+	// 	}
+
+	// 	cout << endl;
+	// 	alp++;
+	// }
+
+	// or
+
+	// for (int row = 1; row <= n; row++)
+	// {
+	// 	char alp = 'a' + (row - 1);
+
+	// 	for (int col = 1; col <= n; col++)
+	// 	{
+	// 		cout << alp;
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	// or
+
+	// for (char row = 'a'; row <= 'e'; row++)
+	// {
+	// 	for (int col = 1; col <= 5; col++)
+	// 	{
+	// 		cout << row;
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	// abcde
+	// abcde
+	// abcde
+	// abcde
+	// abcde
+
+	// int n = 5;
+
+	// for (int row = 1; row <= n; row++)
+	// {
+	// 	char alp = 'a';
+
+	// 	for (int col = 1; col <= n; col++)
+	// 	{
+	// 		cout << alp;
+	// 		alp++;
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	// or
+
+	// for (int row = 1; row <= n; row++)
+	// {
+	// 	for (int col = 1; col <= n; col++)
+	// 	{
+	// 		char alp = 'a' + (col - 1);
+	// 		cout << alp;
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	// or
+
+	// for (int row = 1; row <= n; row++)
+	// {
+	// 	for (char col = 'a'; col <= 'e'; col++)
+	// 	{
+	// 		cout << col;
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	// 1 2 3 4 5
+	// 6 7 8 9 10
+	// 11 12 13 14 15
+	// 16 17 18 19 20
+	// 21 22 23 24 25
+
+	// int n = 5;
+	// int count = 1;
+
+	// for (int row = 1; row <= n; row++)
+	// {
+	// 	for (int col = 1; col <= n; col++)
+	// 	{
+	// 		cout << count << " ";
+	// 		count++;
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	// or
+
+	// for (int row = 1; row <= n; row++)
+	// {
+	// 	for (int col = 1; col <= n; col++)
+	// 	{
+	// 		cout << (row - 1) * 5 + col << " ";
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+ row <= n; row++)
+	// {
+	// 	char alp = 'a';
+
+	// 	for (int col = 1; col <= n; col++)
+	// 	{
+	// 		cout << alp;
+	// 		alp++;
+	// 	}
+
+	// 	cout << endl;
+	// }
