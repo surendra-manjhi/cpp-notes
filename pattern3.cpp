5aa492d2e8500ed2b96069d2940222799a8b4f3c
Surendra <manjhisurendra08@gmail.com>
2024-03-27 14:51:27 +0530

    Added pattern part 3

---
+++ pattern3.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//     *
+	//    **
+	//   ***
+	//  ****
+	// *****
+
+	// for (int row = 1; row <= 5; row++)
+	// {
+	// 	for (int spc = 1; spc <= 5 - row; spc++)
+	// 	{
+	// 		cout << " "; // space
+	// 	}
+
+	// 	for (int prnt = 1; prnt <= row; prnt++)
+	// 	{
+	// 		cout << "*"; // star
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	//     1
+	//    22
+	//   333
+	//  4444
+	// 55555
+
+	// for (int row = 1; row <= 5; row++)
+	// {
+	// 	for (int spc = 1; spc <= 5 - row; spc++)
+	// 	{
+	// 		cout << " "; // space
+	// 	}
+
+	// 	for (int prnt = 1; prnt <= row; prnt++)
+	// 	{
+	// 		cout << row; 
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	//     1
+	//    12
+	//   123
+	//  1234
+	// 12345
+
+	// for (int row = 1; row <= 5; row++)
+	// {
+	// 	for (int spc = 1; spc <= 5 - row; spc++)
+	// 	{
+	// 		cout << " "; // space
+	// 	}
+
+	// 	for (int prnt = 1; prnt <= row; prnt++)
+	// 	{
+	// 		cout << prnt; 
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	//     A
+	//    AB
+	//   ABC
+	//  ABCD
+	// ABCDE
+
+	// for (int row = 1; row <= 5; row++)
+	// {
+	// 	for (int spc = 1; spc <= 5 - row; spc++)
+	// 	{
+	// 		cout << " "; // space
+	// 	}
+
+	// 	for (char prnt = 'A'; prnt <= 'A' + (row - 1); prnt++)
+	// 	{
+	// 		cout << prnt; 
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	//     1
+	//    21
+	//   321
+	//  4321
+	// 54321
+
+	// for (int row = 1; row <= 5; row++)
+	// {
+	// 	for (int spc = 1; spc <= 5 - row; spc++)
+	// 	{
+	// 		cout << " "; // space
+	// 	}
+
+	// 	for (int prnt = row; prnt >= 1; prnt--)
+	// 	{
+	// 		cout << prnt;
+	// 	}
+
+	// 	cout << endl;
+	// }
+
+	return 0;
+}
